749a28e9be24c491a3305bd604108a37abc5c735
jason <jasonca1@student.ubc.ca>
2023-05-18 20:52:08 -0700

    Ignoring audio_files contents

--- transcriber.py
+++ transcriber.py
@@ -1,6 +1,17 @@
 import speech_recognition as sr
+import os
 
-audio_file = "audio_files/untitled.wav"
+# Path to the audio files folder
+folder_path = "audio_files"
+
+# Get the list of audio files in the folder
+audio_files = []
+for file_name in os.listdir(folder_path):
+    if file_name.endswith(".wav"):
+        file_path = os.path.join(folder_path, file_name)
+        audio_files.append(file_path)
+
+print(audio_files)
 
 # Create a recognizer object and load the audio file
 recognizer = sr.Recognizer()
